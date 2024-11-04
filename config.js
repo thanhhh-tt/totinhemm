7ca197ea687401e209a046cb0525225d7f8e5c83
thanhhh-tt <xuyenthanhdat1980@gmail.com>
2024-11-04 18:40:51 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    introTitle: 'Công chúa à!',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: 'hihi',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Vẫn cứ là thích anh <33',
+    btnNo: 'Không, Anh trai à :3',
+    question:'Trên thế giới hơn 7 tỉ người, mà sao con tim anh chỉ nghĩ tới em. Em có thể cho anh cơ hội che chở cho em được không?? <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Tối nay hai đứa mình cùng chơi game nhé công chúa.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.messenger.com/e2ee/t/5479689735489682'
+}
